7266bb310eee9d690b2bcfe5c0753e17b9e31800
non <non@okakam.net>
2016-03-20 09:26:21 +0000

    サンプルコード追加

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 /**
  * Created by non on 2016/03/20.
  */
+
+"use strict";
+
+var Cylon = require("cylon");
+
+Cylon.robot({
+  connections: {
+    raspi: { adaptor: "raspi", port: "/dev/pi-blaster" }
+  },
+
+  devices: {
+    servo: {
+      driver: "servo",
+      pin: 11,
+      limits: { bottom: 20, top: 160 }
+    }
+  },
+
+  work: function(my) {
+    var angle = 30,
+        increment = 5;
+
+    every((1).seconds(), function() {
+      angle += increment;
+      my.servo.angle(angle);
+      console.log("Current Angle: " + (my.servo.currentAngle()));
+
+      if ((angle === 30) || (angle === 150)) { increment = -increment; }
+    });
+  }
+}).start();
